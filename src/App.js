6b37e63f19edbfda28d470273bbd75e9cc5ac19a
Ardee Aram <ardee.aram@gmail.com>
2021-01-14 01:54:43 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect} from 'react';
+import { authenticate } from './FacebookSDK';
+
+const App = () => { 
+  const [fbId, setFbId]= useState();
+  const [displayName, setDisplayName] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const [imageHeight, setImageHeight] = useState();
+  const [imageWidth, setImageWidth] = useState();
+  const [loggedIn, setLoggedIn] = useState(false);
+  
+  const processAuthResponse = async (authResponse) => {
+      const { accessToken } = authResponse;
+      const authenticateResponse = await authenticate(accessToken);
+      const { data } = authenticateResponse;
+      const { name, id, picture } = data;
+     
+      setFbId(id);
+      setDisplayName(name);
+      setImageUrl(picture.data.url);
+      setLoggedIn(true);
+    
+  }
+  
+  const fbLogin = () => {
+    (async () => {
+      const { authResponse } = await new Promise(window.FB.login);
+      await processAuthResponse(authResponse);
+    })();
+  };
+  
+  const fbLogout = () => {
+    (async () => {
+      await window.FB.logout();
+      setLoggedIn(false);
+    })();
+  }
+  
+  useEffect(() => {
+      // auto authenticate with the api if already logged in with facebook
+      window.FB.getLoginStatus(({ authResponse }) => {
+        (async () => {
+            if (authResponse) {
+                await processAuthResponse(authResponse);
+            } else {
+                setLoggedIn(false);
+            }
+            
+        })();
+      });
+  }, []);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        { loggedIn &&
+          <div>
+            <div className='fb-login-form'>
+              <img className='fb-image' src={imageUrl} />
+              <div>ID: {fbId || '-'} </div>
+              <div>Display Name: {displayName || '-'} </div>
+            </div>
+            <button className="btn-facebook" onClick={fbLogout}>Logout</button>
+          </div>
+        }
+        
+        {!loggedIn &&
+          <div>
+            <button className="btn-facebook" onClick={fbLogin}>Login with Facebook</button>
+          </div>
+        }
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FacebookSDK.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+const facebookAppId = process.env.REACT_APP_FACEBOOK_APP_ID;
+
+export const initializeFacebookSDK = () => {
+    return new Promise(resolve => {
+      
+        // wait for facebook sdk to initialize before starting the react app
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: facebookAppId,
+                cookie: true,
+                xfbml: true,
+                version: 'v8.0'
+            });
+            console.log("Successfully initialized Facebook SDK.");
+
+            // let the app handle what happens after login
+            // the app will hang if this is not resolved properly.
+            window.FB.getLoginStatus(({ authResponse }) =>  resolve(authResponse));
+        };
+
+        // load facebook sdk script
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));    
+    });
+}
+
+export const authenticate = async (accessToken) => {
+    
+    const response = await axios.get(`https://graph.facebook.com/v8.0/me?access_token=${accessToken}&fields=id,name,email,picture.width(640).height(640)`);
+    return response;
+    
+}
+
+export const logout = async () => {
+    
+    
+}
